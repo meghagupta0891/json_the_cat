c55f05a73d222fbddf5c7957088f6e95f733bb04
Megha <meghagupta0891@gmail.com>
2021-01-14 22:05:01 +0000

    Added a mocha test

--- breed-fetcher.js
+++ breed-fetcher.js
@@ -1,14 +1,18 @@
-const input = process.argv.slice(2);
 const request = require("request");
-const url = "https://api.thecatapi.com/v1/breeds/search?q="+input;
-request(url, (error, res, body) => {
-    const data = JSON.parse(body);
-    const breed = data[0];
-    if (error) {
-      console.log('error');
-    } else if (breed == null) {
-      console.log('This breed does not exist');
-    } else {
-      console.log(breed.description);
-    }
-});
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = "https://api.thecatapi.com/v1/breeds/search?q="+breedName;
+    request(url, (error, res, body) => {
+        const data = JSON.parse(body);
+        breedName = data[0];
+        if (error) {
+          callback(error,null);
+        } else if (breedName == null) {
+          callback('This breed does not exist',null);
+        } else {
+          callback(null,breedName.description);
+        }
+    });
+};
+module.exports = { fetchBreedDescription };
